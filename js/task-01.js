3c39c93a1e07a504e07bbfb181c8ed315c049eaf
Dima-1990 <99199420+Dima-1990@users.noreply.github.com>
2023-05-17 20:27:11 +0300

    first task is completed

---
+++ js/task-01.js
@@ -0,0 +1,17 @@
+
+const myCategoriEl = document.querySelector("#categories");
+
+
+ console.log(`Number of categories:${myCategoriEl.children.length}`);
+
+
+[...myCategoriEl.children].forEach((category) => {
+   
+    const categoryElements = category.lastElementChild.children;
+
+    console.log(
+     `Category: ${category.firstElementChild.textContent}
+      Elements: ${categoryElements.length}`
+    );
+ });
+
